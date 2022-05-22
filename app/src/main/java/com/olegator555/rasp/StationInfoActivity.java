ebec370327c2caad3db8ed9db0b2b9f851272dd2
olegator <o.shashko@icloud.com>
2022-05-22 20:21:43 +0300

    webView fixed

--- app/src/main/java/com/olegator555/rasp/StationInfoActivity.java
+++ app/src/main/java/com/olegator555/rasp/StationInfoActivity.java
@@ -4,20 +4,22 @@ import android.annotation.SuppressLint;
 import android.net.http.SslError;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.webkit.JavascriptInterface;
 import android.webkit.SslErrorHandler;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
 
 import java.util.ArrayList;
 
 public class StationInfoActivity extends AppCompatActivity {
     private TextView tempTextView;
     private String url;
+    private ProgressBar progressBar;
+    private WebView webView;
     private ArrayList<String> list;
     @SuppressLint({"JavascriptInterface", "SetJavaScriptEnabled"})
     @JavascriptInterface
@@ -26,6 +28,7 @@ public class StationInfoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_station_info);
         tempTextView = findViewById(R.id.tempTextView);
+        progressBar = findViewById(R.id.webVIewProgressBar);
         String stationName = getIntent().getStringExtra("StationName");
         url = "https://central-ppk.ru/new/imap/map_cl.php?searching=1&searchstation=" + stationName;
         Log.d("Url", url);
@@ -45,19 +48,20 @@ public class StationInfoActivity extends AppCompatActivity {
                 tempTextView.setText(list.toString());
             }
         }.start();*/
-        WebView webview = new WebView(this);
-        webview.getSettings().setJavaScriptEnabled(true);
-        webview.getSettings().setDomStorageEnabled(true);
-        webview.getSettings().setSaveFormData(true);
-        webview.getSettings().setSavePassword(true);
-        webview.getSettings().setLoadsImagesAutomatically(true);
-        webview.getSettings().setUserAgentString("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36");
-        webview.addJavascriptInterface(new MyJavaScriptInterface(), "htmlHandler");
-        webview.setWebViewClient(new WebViewClient() {
+        webView = findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setSaveFormData(true);
+        webView.getSettings().setSavePassword(true);
+        webView.getSettings().setLoadsImagesAutomatically(true);
+        webView.getSettings().setUserAgentString("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36");
+        webView.addJavascriptInterface(new MyJavaScriptInterface(), "HTMLOUT");
+        webView.setWebViewClient(new WebViewClient() {
             @Override
             public void onPageFinished(WebView view, String url) {
-                webview.loadUrl("javascript:window.htmlHandler.handleHtml" +
-                        "('<html>'+document.getElementsByTagName('script')[0]+'</html>');");
+                progressBar.setVisibility(View.GONE);
+                webView.setVisibility(View.VISIBLE);
+                //webview.loadUrl("javascript:window.HTMLOUT.handleHtml(document.getElementsByClassName('ymaps'));");
             }
 
             @Override
@@ -66,13 +70,14 @@ public class StationInfoActivity extends AppCompatActivity {
             }
 
         });
-        webview.loadUrl(url);
+        webView.loadUrl(url);
 
     }
     private static class MyJavaScriptInterface {
         @JavascriptInterface
         public void handleHtml(String html) {
-            Document doc = Jsoup.parse(html);
+            Log.d("Caller", "CALLED!");
+            //Document doc = Jsoup.parse(html);
             Log.d("html", html);
         }
     }
