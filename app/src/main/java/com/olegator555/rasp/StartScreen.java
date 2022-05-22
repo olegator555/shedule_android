96b56a3ed4cf8853cf49e4608c88a045be92a4c4
olegator <o.shashko@icloud.com>
2022-05-22 21:31:24 +0300

    stat screen image added

--- app/src/main/java/com/olegator555/rasp/StartScreen.java
+++ app/src/main/java/com/olegator555/rasp/StartScreen.java
@@ -1,11 +1,11 @@
 package com.olegator555.rasp;
 
-import Utils.AppLevelUtilsAndConstants;
 import Utils.GetRequest;
 import Utils.UrlCreator;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
@@ -14,25 +14,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import static Utils.AppLevelUtilsAndConstants.PreferencesKeys.IS_MAIN_ACTIVITY_VISITED;
 import static Utils.AppLevelUtilsAndConstants.PreferencesKeys.JSON_STRING_KEY;
+import static Utils.AppLevelUtilsAndConstants.readFromPreferences;
+import static Utils.AppLevelUtilsAndConstants.writeToPreferences;
 
 
 public class StartScreen extends AppCompatActivity {
     ProgressBar progressBar;
     TextView textView;
+    private int splashDuration;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
         progressBar = findViewById(R.id.progressBar2);
-        progressBar.setVisibility(View.VISIBLE);
         textView = findViewById(R.id.textView);
-        if((Boolean)AppLevelUtilsAndConstants.readFromPreferences(this, IS_MAIN_ACTIVITY_VISITED,
+        if((Boolean) readFromPreferences(this, IS_MAIN_ACTIVITY_VISITED,
                 Boolean.class)) {
-            Intent intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
-            Log.d("Bool", "already written, redirect");
+            splashDuration = 100;
+            new Handler().postDelayed(() -> {
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                Log.d("Bool", "already written, redirect");
+            }, splashDuration);
         }
         else {
+            progressBar.setVisibility(View.VISIBLE);
             Log.d("Bool", "Not written yet");
             GetRequest stations_list_request = new StationLisRequest();
             stations_list_request.execute(new UrlCreator().getStationsListUrl());
@@ -61,7 +67,7 @@ public class StartScreen extends AppCompatActivity {
         protected void onPostExecute(String res) {
             super.onPostExecute(res);
             Intent intent = new Intent(StartScreen.this, First_launch.class);
-            AppLevelUtilsAndConstants.writeToPreferences(getBaseContext(), JSON_STRING_KEY, res, String.class);
+            writeToPreferences(getBaseContext(), JSON_STRING_KEY, res, String.class);
             startActivity(intent);
         }
     }
